b2e0567f5317efcac831cd3d3b5c4ada9528db6b
olivierblanc42 <olivierhumanbooster@gmail.com>
2024-12-27 15:50:11 +0100

    debut projet partie back

---
+++ manga_up_back/manga_up/src/main/java/manga_up/manga_up/MangaUpApplication.java
@@ -0,0 +1,13 @@
+package manga_up.manga_up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaUpApplication.class, args);
+	}
+
+}
